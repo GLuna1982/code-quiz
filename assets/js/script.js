5e0bc8d6f3b7fa60a4c2e88f2705ed4890803e5a
GLuna1982 <gluna0705@gmail.com>
2022-06-19 22:26:03 -0400

    question and answer show after start quiz button is clicked

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var questionEl = document.querySelector("#q1");
+var buttonQsEl = document.querySelector("#hide-btn");
+var counter = 90;
+function show () {
+    document.getElementById("hide").id = "q-main";
+    document.getElementById("hide-btn").id = "view-btn";
+    document.getElementById("main-section").id = "hide";
+    document.getElementById("main-btn").id = "hide-btn";
+
+    
+    var countdown = setInterval(function() {
+        if(counter > 0) {
+        var count = counter;
+        counter--; 
+        document.getElementById("timer").innerHTML = count + " seconds";
+        } 
+        else {
+        clearInterval(countdown);
+        document.getElementById("timer").innerHTML = "Time Out";
+        }
+    }, 1000);
+};
+
+questionEl.textContent = "Inside which HTML element do we put the JavaScript?";
+    var answerArray = ["<javascript>","<script>","<link>","<js>"];
+    for (i=0; i<answerArray.length; i++) {
+        var answerEl = document.createElement("button");
+        answerEl.className = "btn";
+        answerEl.textContent = answerArray[i];
+        console.log (answerEl);
+        buttonQsEl.appendChild(answerEl);
+    }
+    if (answerEl.textContent == answerArray[1]) {
+        changeQ();
+    }
+    else {
+        
+    }
+//Function adds questions to Question section
+function changeQ () {
+
+};
+
+changeQ();
+
+//Event listener for 'start quiz' button
+document.getElementById("start").addEventListener("click", show);
